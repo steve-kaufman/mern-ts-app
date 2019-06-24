0abbb509bd24dfb726e74a2d1410c7dab5d54292
Steven Kaufman <steveplaysguiola@gmail.com>
2019-06-24 15:38:58 -0500

    added redux boilerplate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+
+import store from './store'
 
 export default class App extends Component {
   render () {
     return (
-      <div>
-          Hello
-      </div>
+      <Provider store={store}>
+        <div>
+            Hello
+        </div>
+      </Provider>
     )
   }
 }
